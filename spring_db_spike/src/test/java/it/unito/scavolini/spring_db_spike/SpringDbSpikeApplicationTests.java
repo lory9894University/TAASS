431c1ac669d1d59e5ec9c9bd0d576423d4b98db0
Lorenzo Dentis <lory9894@gmail.com>
2022-11-08 22:27:38 +0100

    added submodule CommandApp

---
+++ spring_db_spike/src/test/java/it/unito/scavolini/spring_db_spike/SpringDbSpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unito.scavolini.spring_db_spike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbSpikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
